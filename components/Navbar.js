1b68b57dd2c2eaee965927003a830424e375ce0b
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-07-08 16:17:28 -0400

    added logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,17 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>Recipe List</h1>
+        <Link href="/">
+          <Image src="/my-recipes.svg" width={250} height={95} />
+        </Link>
       </div>
       <Link href="/"><a>Home</a></Link>
       <Link href="/about"><a>About</a></Link>
-      <Link href="/recipes"><a>Recipe Listing</a></Link>
+      <Link href="/recipes"><a>Recipe List</a></Link>
       
       
     </nav>
