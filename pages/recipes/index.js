3bb032ffec61ce1924e1abd1da8a9fa380b1b2bf
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-07-08 16:18:59 -0400

    recipes list page

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,8 +1,37 @@
-const Recipes = () => {
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../../styles/Recipes.module.css';
+
+//runs at build time, not in the browser
+export const getStaticProps = async () => {
+  const resp = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=s');
+  const data = await resp.json();
+  
+  return {
+    props: { recipes: data.meals }
+  }
+}
+
+
+const Recipes = ({ recipes }) => {
+
   return (
-    <div>
-      <h1>All Recipes</h1>
-    </div>
+    <>
+      <Head>
+        <title>Recipe List | Recipes</title>
+        <meta name="keywords" content="recipes"/>
+      </Head>
+      <div>
+        <h1>Recipe List</h1>
+        {recipes.map(recipe => (
+          <Link href={'/recipes/' + recipe.idMeal} key={recipe.idMeal}>
+            <a className={styles.single}>
+              <h3>{recipe.strMeal}</h3>
+            </a>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
  
